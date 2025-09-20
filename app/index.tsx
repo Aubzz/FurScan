483d5a28e445402801f95b3bcf37d8661674b671
Aubrey <ragandanaubrey@gmail.com>
2025-09-20 18:16:49 +0800

    Starting

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
+      {/* <Link href="/Screens/StartScreen">Start Screen Page</Link> */}
+    </View>
+  );
+}
